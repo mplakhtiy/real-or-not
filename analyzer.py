97fa591716e4a9cd47aa2caac8af41dc56b13449
Maryan Plakhtiy <mplakhtiy@gmail.com>
2020-09-08 15:37:42 +0200

    analysis

--- analyzer.py
+++ analyzer.py
@@ -1,37 +1,102 @@
 import pandas as pd
 
-data = pd.read_csv('./tds/tds-27m.csv')
+data = pd.read_csv('./7000-10fcv-27m-TDS.csv')
 
 data_dict = data.to_dict()
 
-BERT = ['BERT-8b7db91c']
-GLOVE_MODELS = [
-    'LSTM-GloVe-8b7db91c', 'LSTM_DROPOUT-GloVe-8b7db91c', 'BI_LSTM-GloVe-8b7db91c', 'LSTM_CNN-GloVe-8b7db91c',
-    'FASTTEXT-GloVe-8b7db91c', 'RCNN-GloVe-8b7db91c', 'CNN-GloVe-8b7db91c', 'RNN-GloVe-8b7db91c',
-    'GRU-GloVe-8b7db91c', 'HAN-GloVe-8b7db91c',
-]
-MODELS = [
-    'LSTM-71bd09db', 'LSTM_DROPOUT-d3cc3c6e', 'BI_LSTM-8b7db91c', 'LSTM_CNN-8b7db91c', 'FASTTEXT-b054e509',
-    'RCNN-7bc816a1', 'CNN-b054e509', 'RNN-1258a9d2', 'GRU-b054e509', 'HAN-a85c8435'
-]
-CLASSIFIERS = [
-    'RIDGE-2e359f0b', 'SVC-4c2e484d', 'LOGISTIC_REGRESSION-8b7db91c', 'SGD-2e359f0b',
-    'DECISION_TREE-7bc816a1', 'RANDOM_FOREST-60314ef9'
-]
+# BERT = ['BERT-8b7db91c']
+# GLOVE_MODELS = [
+#     'LSTM-GloVe-8b7db91c', 'LSTM_DROPOUT-GloVe-8b7db91c', 'BI_LSTM-GloVe-8b7db91c', 'LSTM_CNN-GloVe-8b7db91c',
+#     'FASTTEXT-GloVe-8b7db91c', 'RCNN-GloVe-8b7db91c', 'CNN-GloVe-8b7db91c', 'RNN-GloVe-8b7db91c',
+#     'GRU-GloVe-8b7db91c', 'HAN-GloVe-8b7db91c',
+# ]
+# MODELS = [
+#     'LSTM-71bd09db', 'LSTM_DROPOUT-d3cc3c6e', 'BI_LSTM-8b7db91c', 'LSTM_CNN-8b7db91c', 'FASTTEXT-b054e509',
+#     'RCNN-7bc816a1', 'CNN-b054e509', 'RNN-1258a9d2', 'GRU-b054e509', 'HAN-a85c8435'
+# ]
+# CLASSIFIERS = [
+#     'RIDGE-2e359f0b', 'SVC-4c2e484d', 'LOGISTIC_REGRESSION-8b7db91c', 'SGD-2e359f0b',
+#     'DECISION_TREE-7bc816a1', 'RANDOM_FOREST-60314ef9'
+# ]
+#
+# result = {
+#     'id': [],
+#     'keyword': [],
+#     'location': [],
+#     'text': [],
+#     'target': [],
+#     'Correct Prediction': [],
+# }
 
-result = {
+result= {
     'id': [],
-    'keyword': [],
-    'location': [],
+    'index': [],
+    'fold': [],
     'text': [],
     'target': [],
-    'Correct Prediction': [],
+    'BERT-2e359f0b': [],
+    'LSTM-GloVe-8b7db91c': [],
+    'LSTM_DROPOUT-GloVe-8b7db91c': [],
+    'BI_LSTM-GloVe-8b7db91c': [],
+    'LSTM_CNN-GloVe-8b7db91c': [],
+    'FASTTEXT-GloVe-8b7db91c': [],
+    'RCNN-GloVe-71bd09db': [],
+    'CNN-GloVe-d3cc3c6e': [],
+    'RNN-GloVe-7bc816a1': [],
+    'GRU-GloVe-8b7db91c': [],
+    'HAN-GloVe-a85c8435': [],
+    'LSTM-71bd09db': [],
+    'LSTM_DROPOUT-d3cc3c6e': [],
+    'BI_LSTM-8b7db91c': [],
+    'LSTM_CNN-8b7db91c': [],
+    'FASTTEXT-b054e509': [],
+    'RCNN-7bc816a1': [],
+    'CNN-b054e509': [],
+    'RNN-1258a9d2': [],
+    'GRU-b054e509': [],
+    'HAN-a85c8435': [],
+    'RIDGE-2e359f0b': [],
+    'SVC-4c2e484d': [],
+    'LOGISTIC_REGRESSION-8b7db91c': [],
+    'SGD-2e359f0b': [],
+    'DECISION_TREE-7bc816a1': [],
+    'RANDOM_FOREST-60314ef9': []
 }
 
-KEYS = BERT + ['LSTM_DROPOUT-GloVe-8b7db91c', 'GRU-GloVe-8b7db91c']
+# KEYS = BERT + ['LSTM_DROPOUT-GloVe-8b7db91c', 'GRU-GloVe-8b7db91c']
+#
+# for k in KEYS:
+#     result[k] = []
 
-for k in KEYS:
-    result[k] = []
+KEYS = [
+    'BERT-2e359f0b',
+    'LSTM-GloVe-8b7db91c',
+    'LSTM_DROPOUT-GloVe-8b7db91c',
+    'BI_LSTM-GloVe-8b7db91c',
+    'LSTM_CNN-GloVe-8b7db91c',
+    'FASTTEXT-GloVe-8b7db91c',
+    'RCNN-GloVe-71bd09db',
+    'CNN-GloVe-d3cc3c6e',
+    'RNN-GloVe-7bc816a1',
+    'GRU-GloVe-8b7db91c',
+    'HAN-GloVe-a85c8435',
+    'LSTM-71bd09db',
+    'LSTM_DROPOUT-d3cc3c6e',
+    'BI_LSTM-8b7db91c',
+    'LSTM_CNN-8b7db91c',
+    'FASTTEXT-b054e509',
+    'RCNN-7bc816a1',
+    'CNN-b054e509',
+    'RNN-1258a9d2',
+    'GRU-b054e509',
+    'HAN-a85c8435',
+    'RIDGE-2e359f0b',
+    'SVC-4c2e484d',
+    'LOGISTIC_REGRESSION-8b7db91c',
+    'SGD-2e359f0b',
+    'DECISION_TREE-7bc816a1',
+    'RANDOM_FOREST-60314ef9'
+]
 
 for index in data_dict['id']:
     has_diff = False
@@ -44,13 +109,16 @@ for index in data_dict['id']:
             # break
 
     # if has_diff:
-    if len(l) != len(KEYS) and len(l) != 0:
+    if len(l) == len(KEYS) and len(l) != 0:
+        # result['id'].append(data_dict['id'][index])
         result['id'].append(data_dict['id'][index])
-        result['keyword'].append(data_dict['keyword'][index])
-        result['location'].append(data_dict['location'][index])
+        result['fold'].append(data_dict['fold'][index])
+        result['index'].append(data_dict['index'][index])
+        # result['keyword'].append(data_dict['keyword'][index])
+        # result['location'].append(data_dict['location'][index])
         result['text'].append(data_dict['text'][index])
         result['target'].append(data_dict['target'][index])
-        result['Correct Prediction'].append(data_dict['Correct Prediction'][index])
+        # result['Correct Prediction'].append(data_dict['Correct Prediction'][index])
         for key in KEYS:
             result[key].append(data_dict[key][index])
 
@@ -58,4 +126,4 @@ a = {}
 for k, v in result.items():
     a[k] = pd.Series(v)
 a = pd.DataFrame(a)
-a.to_csv(f'./tds/tds-3t-some-failed.csv', index=False)
+a.to_csv(f'./7000-10fcv-27m-TDS-failed.csv', index=False)
--- get_analysis.py
+++ get_analysis.py
@@ -3,8 +3,9 @@ from pathlib import Path
 from data import train_data, test_data
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
+import pandas as pd
 
-folder = '10000'
+folder = '7000'
 
 logs = [str(lst) for lst in list(Path(f"./logs/bert/{folder}").rglob("*.json"))] + \
        [str(lst) for lst in list(Path(f"./logs/keras/{folder}").rglob("*.json"))] + \
@@ -17,11 +18,11 @@ logs = [str(lst) for lst in list(Path(f"./logs/bert/{folder}").rglob("*.json"))]
 def get_best_epoch(history):
     best_loss_index = history['val_loss'].index(min(history['val_loss']))
     temp_acc = 0
-    temp_loss = 0
+    temp_loss = 99999
     best_acc_index = 0
 
     for i in range(best_loss_index + 1):
-        if temp_acc <= history['val_accuracy'][i] and temp_loss <= history['val_loss'][i]:
+        if temp_acc <= history['val_accuracy'][i] and history['val_loss'][i] <= temp_loss:
             temp_acc = history['val_accuracy'][i]
             temp_loss = history['val_loss'][i]
             best_acc_index = i
@@ -46,9 +47,12 @@ for log_path in logs:
                 "accuracy": fold_history['accuracy'],
                 "val_loss": fold_history['val_loss'],
                 "val_accuracy": fold_history['val_accuracy'],
+                "test_loss": fold_history['test_loss'],
+                "test_accuracy": fold_history['test_accuracy'],
                 "best_acc_epoch": best_acc_epoch,
                 "best_loss_epoch": best_loss_epoch,
-                "val_predictions": fold_history['val_predictions'][best_acc_epoch]
+                "val_predictions": fold_history['val_predictions'][best_acc_epoch],
+                "test_predictions": fold_history['test_predictions'][best_acc_epoch]
             }
 
             kfold_history.append(new_fold)
@@ -59,20 +63,82 @@ for log_path in logs:
 
             new_log[log_path] = log_copy
 
-save_to_file(f'./{folder}-analysis.json', new_log)
+save_to_file(f'./{folder}-10fcv-27m.json', new_log)
 
-# SEED = 7
-# KFOLD = 10
-#
-# kfold = StratifiedKFold(n_splits=KFOLD, shuffle=True, random_state=SEED)
-#
+SEED = 7
+KFOLD = 10
+
+kfold = StratifiedKFold(n_splits=KFOLD, shuffle=True, random_state=SEED)
+
 # inputs = np.concatenate([train_data['text'], test_data.text])
 # targets = np.concatenate([train_data['target'], test_data.target])
-#
-#
-#
+# inputs = train_data['text']
+# targets = train_data['target']
+inputs = test_data['text']
+targets = test_data['target']
+
+result_csv = {
+    'index': [],
+    'fold': [],
+    'text': [],
+    'target': [],
+    'BERT-2e359f0b': [],
+    'LSTM-GloVe-8b7db91c': [],
+    'LSTM_DROPOUT-GloVe-8b7db91c': [],
+    'BI_LSTM-GloVe-8b7db91c': [],
+    'LSTM_CNN-GloVe-8b7db91c': [],
+    'FASTTEXT-GloVe-8b7db91c': [],
+    'RCNN-GloVe-71bd09db': [],
+    'CNN-GloVe-d3cc3c6e': [],
+    'RNN-GloVe-7bc816a1': [],
+    'GRU-GloVe-8b7db91c': [],
+    'HAN-GloVe-a85c8435': [],
+    'LSTM-71bd09db': [],
+    'LSTM_DROPOUT-d3cc3c6e': [],
+    'BI_LSTM-8b7db91c': [],
+    'LSTM_CNN-8b7db91c': [],
+    'FASTTEXT-b054e509': [],
+    'RCNN-7bc816a1': [],
+    'CNN-b054e509': [],
+    'RNN-1258a9d2': [],
+    'GRU-b054e509': [],
+    'HAN-a85c8435': [],
+    'RIDGE-2e359f0b': [],
+    'SVC-4c2e484d': [],
+    'LOGISTIC_REGRESSION-8b7db91c': [],
+    'SGD-2e359f0b': [],
+    'DECISION_TREE-7bc816a1': [],
+    'RANDOM_FOREST-60314ef9': []
+}
+
+for key in new_log:
+    if '/sklearn' in key:
+        csv_key = f'{new_log[key]["CLASSIFIER"]["TYPE"]}-{new_log[key]["PREPROCESSING_ALGORITHM_UUID"]}'
+    else:
+        if 'GLOVE' in new_log[key]:
+            csv_key = f'{new_log[key]["TYPE"]}-GloVe-{new_log[key]["PREPROCESSING_ALGORITHM_UUID"]}'
+        else:
+            csv_key = f'{new_log[key]["TYPE"]}-{new_log[key]["PREPROCESSING_ALGORITHM_UUID"]}'
+    for fold in new_log[key]['KFOLD_HISTORY']:
+        result_csv[csv_key] = result_csv[csv_key] + fold['test_predictions']
+        # result_csv[csv_key].append('*')
+
 # for train, validation in kfold.split(inputs, targets):
-#     x_train = inputs[train]
-#     y_train = targets[train]
-#     x_val = inputs[validation]
-#     y_val = targets[validation]
+#     result_csv['index'] = result_csv['index'] + validation.tolist()
+#     result_csv['text'] = result_csv['text'] + inputs[validation].tolist()
+#     result_csv['target'] = result_csv['target'] + targets[validation].tolist()
+#     # result_csv['index'].append('*')
+#     # result_csv['text'].append('*')
+#     # result_csv['target'].append('*')
+
+for i in range(10):
+    result_csv['index'] = result_csv['index'] + list(range(test_data.text.size))
+    result_csv['fold'] = result_csv['fold'] + [i] * test_data.text.size
+    result_csv['text'] = result_csv['text'] + test_data.text.tolist()
+    result_csv['target'] = result_csv['target'] + test_data.target.tolist()
+
+csv_file = {}
+for k, v in result_csv.items():
+    csv_file[k] = pd.Series(v)
+a = pd.DataFrame(csv_file)
+a.to_csv(f'./{folder}-10fcv-27m-TDS.csv', index=True)
